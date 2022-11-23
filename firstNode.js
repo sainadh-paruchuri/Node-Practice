1ca6c9bad9381f84bfdaa9eb82284f153eb58d9c
sainadh-paruchuri <saiparuchuri121@gmail.com>
2022-11-23 12:28:46 +0530

    objects

--- firstNode.js
+++ firstNode.js
@@ -1,1 +1,28 @@
 console.log('hello world')
+
+let person={
+    name:'sai',
+    age:21,
+    hobbies:['reading','writing','watchiing']
+ 
+}
+//if you want reference
+// let secondPerson=person;
+// secondPerson.name='hari';
+// console.log(person)
+//console.log(secondPerson)
+
+//if you want copy
+let secondPerson=Object.assign({},person);
+secondPerson.name='hari';
+console.log(person)
+console.log(secondPerson)
+
+var hobbies=person.hobbies;
+var hobbiess=person.hobbies.slice();
+console.log(hobbiess)
+
+person.hobbies.push('travelling')
+
+console.log(hobbies)
+console.log(hobbiess)
