902a8b75c2e5815c5df3f63dd7e32d394ceacf18
sainadh-paruchuri <saiparuchuri121@gmail.com>
2022-11-23 17:30:28 +0530

    async function added

--- firstNode.js
+++ firstNode.js
@@ -50,3 +50,44 @@ let [ a,b,c ]=arr;
 console.log(a)
 console.log(b)
 console.log(c)
+
+// const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+// const { key1, key3}  = { ...obj1}
+// console.log(key1, key3)
+
+const arr1 = ['value1', 'value2']
+const [ val1, val2 ] = arr1
+console.log(val1)
+
+const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+	let { key1, key3}  = obj1
+	key1 = 20;
+    key3 = 123
+	console.log(obj1.key1, obj1.key3)
+    console.log(key1)
+
+async function print(){
+console.log('a');
+console.log('b');
+let result=await new Promise(resolve=>{
+        setTimeout(() => resolve('c'), 3000)
+})
+console.log(result);
+let result1=await new Promise(resolve=>{
+        setTimeout(() => resolve('d'),0)
+})
+console.log(result1);
+console.log('e');
+}
+print()
+
+
+
+
+
+
+
+
+
+
+
