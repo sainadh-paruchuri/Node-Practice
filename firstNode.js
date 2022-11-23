53b08182d8282ce4d67f9a98f931738c3b338939
sainadh-paruchuri <saiparuchuri121@gmail.com>
2022-11-23 13:47:25 +0530

    destructuring added

--- firstNode.js
+++ firstNode.js
@@ -26,3 +26,27 @@ person.hobbies.push('travelling')
 
 console.log(hobbies)
 console.log(hobbiess)
+
+let array= ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+
+let newArray=array.map(arr=>{
+    if(arr==' '){
+        return 'empty string';
+    }else{
+        return arr;
+    }
+})
+console.log(array)
+console.log(newArray)
+
+//destrucring
+
+let [ fruit1,fruit2 ]=array;
+console.log(fruit1);
+console.log(fruit2);
+
+let arr=[1,2,3,4];
+let [ a,b,c ]=arr;
+console.log(a)
+console.log(b)
+console.log(c)
