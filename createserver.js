e015163da8c831cc799dad96003ca69c7cf76bbe
sainadh-paruchuri <saiparuchuri121@gmail.com>
2022-11-24 11:43:01 +0530

    urls added

--- createserver.js
+++ createserver.js
@@ -1,6 +1,38 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log('sainadh')
+   const url=req.url;
+   if(url==="/"){
+    //res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>MY First Page</title></head>')
+    res.write('<body><h1>Hello node.js</h1></body>')
+    res.write('</html>')
+    return res.end();
+   }
+   else if(url==="/home"){
+    res.write('<html>')
+    res.write('<head><title>MY First Page</title></head>')
+    res.write('<body><h1> Welcome home</h1></body>')
+    res.write('</html>')
+     return res.end();
+   }
+   else if(url==="/about"){
+    res.write('<html>')
+    res.write('<head><title>MY First Page</title></head>')
+    res.write('<body><h1> Welcome to About Us page</h1></body>')
+    res.write('</html>')
+     return res.end();
+   }
+   else if(url==="/node"){
+    res.write('<html>')
+    res.write('<head><title>MY First Page</title></head>')
+    res.write('<body><h1> Welcome to my Node Js project</h1></body>')
+    res.write('</html>')
+     return res.end();
+   }
+   
+
+
 })
 server.listen(4000)
 
